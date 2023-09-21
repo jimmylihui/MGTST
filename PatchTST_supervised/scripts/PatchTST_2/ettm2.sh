31e0f3b8fff4105a6928dc041e8a969a2c64060b
LI Jiahui <jiahuili@dycpu5.cse.ust.hk>
2023-09-21 15:44:21 +0800

    a

--- PatchTST_supervised/scripts/PatchTST_2/ettm2.sh
+++
@@ -1,8 +0,0 @@
-data_path_name=ETTm2.csv
-model_id_name=ETTm2
-data_name=ETTm2
-for pred_len in 96 192 336 720
-do
-    python -u /ssddata/data/jiahuili/PatchTST/PatchTST_supervised/run_longExp.py
-      
-done
